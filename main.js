c706f9da9def05d408ce9b0e6cdff13fed60a92a
Antonio Contreras <antoniocontreras71@yahoo.com>
2024-01-10 22:44:32 -0600

    cpu

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const grids = document.getElementsByClassName("grid");
+let startBtn = document.getElementById("gameStartBtn");
 let x = "X";
 let o = "O";
+let values = [0,0,0,0,0,0,0,0];
 function startGame() {
 	const player1ChooseX = document.createElement("p");
 	const player1ChooseXTextNode = document.createTextNode("You will be X. Choose a grid to place an X in");
@@ -9,15 +11,25 @@ function startGame() {
 	playerTurn();
 }
 function playerTurn() {
-	for (let i = 0; i < grids.length;i++) {
-		//console.log(grids[i]);
+	for (let i = 0; i < grids.length-1;i++) {
         grids[i].addEventListener("click", function() {
-			if (grids[i] != 'X' && grids[i] != 'x') {
-				grids[i].innerHTML = 'X';
+			if (grids[i].textContent == 'O' || grids[i].textContent == 'o') {	
+				alert("Pick another grid");
+			} else {
+				grids[i].textContent = 'X';
+				console.log(grids[i].id);
+				values.splice(grids[i].id, 0, grids[i].id);
+				cpuTurn();
 			}
 		});
 	}
 }
 function cpuTurn() {
-	//
+	console.log(values + " should be the same");
+	let openSlots = values;
+	for (let j = 0; j < grids.length-1;j++) {
+		if (grids[i].textContent == 'X' || grids[i].textContent == 'x') {
+			console.log("Player 2 turn " + grids[i].id)
+		}
+	}
 }
