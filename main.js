5afc1679c0e2875440c5f0b05bafb18c9de53e1b
Antonio Contreras <antoniocontreras71@yahoo.com>
2024-01-11 21:08:09 -0600

    function to calculate optimal plays

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ function startGame() {
 	playerTurn();
 }
 function playerTurn() {
-	for (let i = 0; i < grids.length-1;i++) {
+	for (let i = 0; i < grids.length;i++) {
         grids[i].addEventListener("click", function() {
 			if (grids[i].textContent == 'O' || grids[i].textContent == 'o') {	
 				alert("Pick another grid");
 			} else {
 				grids[i].textContent = 'X';
-				console.log(grids[i].id);
+				console.log(grids[i].id + " id of chosen grid");
 				values.splice(grids[i].id, 0, grids[i].id);
 				cpuTurn();
 			}
@@ -27,9 +27,19 @@ function playerTurn() {
 function cpuTurn() {
 	console.log(values + " should be the same");
 	let openSlots = values;
-	for (let j = 0; j < grids.length-1;j++) {
-		if (grids[i].textContent == 'X' || grids[i].textContent == 'x') {
-			console.log("Player 2 turn " + grids[i].id)
+	let allValues= [0,1,2,3,4,5,6,7,8];
+	for (let j = 0; j < grids.length;j++) {
+		if (grids[j].textContent == 'X' || grids[j].textContent == 'x') {
+			allValues.splice(j,1);
+			console.log("The X id should be missing from this: " + allValues);
+			let randomNum = allValues[(Math.floor(Math.random() * allValues.length))];
+			let cpuPick = document.createTextNode("CPU has made their move. Your turn now");
+			document.getElementById("gamePrompts").appendChild(cpuPick);
+			document.getElementById(randomNum).textContent = 'O';
+			playerTurn();
 		}
 	}
+}
+function handleOpenSlots() {
+	//move all stuff of retrieving open grids to here, not inside the other 2 functions
 }
